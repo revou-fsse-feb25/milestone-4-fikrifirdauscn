3dfd232965b1ed4b49534f1f6ae30165c0cf6038
fikrifirdauscn <fikrifcn123@gmail.com>
2025-08-29 16:02:27 +0700

    test(transaction): basic deposit unit test with mocked prisma

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Revo Bank API')
+    .setDescription('Users, Accounts, Transactions')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
 
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
